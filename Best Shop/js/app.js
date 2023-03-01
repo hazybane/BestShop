a3a5d913723eec112702320452215d2fb3119888
hazybane <patukroman@gmail.com>
2023-03-02 00:38:51 +0100

    s

--- Best Shop/js/app.js
+++ Best Shop/js/app.js
@@ -4,7 +4,7 @@ function Calculator(form, summary) {
     orders: 0.25,
     package: {
       basic: 0,
-      professional: 25,
+      professional: 15,
       premium: 60
     },
     accounting: 35,
